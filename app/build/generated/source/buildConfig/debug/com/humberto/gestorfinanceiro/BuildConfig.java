454a028943792c8c1b9e376161505a84a84f975d
Humberto Goncalves <humberto.vtg@gmail.com>
2025-11-24 13:11:56 -0300

    fix: corrigir referencia nao resolvida dateString

--- app/build/generated/source/buildConfig/debug/com/humberto/gestorfinanceiro/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/humberto/gestorfinanceiro/BuildConfig.java
@@ -10,9 +10,9 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
   // Field from default config.
-  public static final String GEMINI_API_KEY = "cAIzaSyBpo9jL9fuGB0NGxuKMNF88lNVQABmF4Cw";
+  public static final String OPENAI_API_KEY = "sk-proj-yDHgmxlyrZ-xxf-Uh3kzQswSlY7p2XLiLPc-";
   // Field from default config.
-  public static final String SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5YmhzemNlZnV4c2RocHZ4Ym5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwMTYwOTYsImV4cCI6MjA2ODU5MjA5Nn0.i3sOjUvpsimEvtO0uDGFNK92IZjcy1VIva_KEBdlZI8";
+  public static final String SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqeGFmY2Fyc3lqcWplc2drdGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3NzkxNjEsImV4cCI6MjA3OTM1NTE2MX0.PIoXveXDjlmWAe0dpvIAsdGRhY0LdG41WZQ5zMg6ld8";
   // Field from default config.
-  public static final String SUPABASE_URL = "https://gybhszcefuxsdhpvxbnk.supabase.co";
+  public static final String SUPABASE_URL = "https://kjxafcarsyjqjesgktjr.supabase.co";
 }
